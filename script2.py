30a6e95ae81e2ccdd19957c7d10dc30142c8036d
ryandkuster <ryan.kuster@gmail.com>
2022-05-13 15:46:05 -0400

    added function and long docstring

--- script2.py
+++ script2.py
@@ -1,1 +1,15 @@
 print ("Second py script file")
+
+
+def simple_square(side):
+    '''
+    this function prints the square of the input integer
+
+    this particular line of docstring is intentionally here because it is not in the best practices of PEP8 and will hopefully be caught by a linter
+    '''
+    square = side**2
+    print(square)
+    return square
+
+
+square = simple_square(4)
